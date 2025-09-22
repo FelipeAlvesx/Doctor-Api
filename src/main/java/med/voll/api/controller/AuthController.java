0bcf987410f1ce5f6668285e65f47d06373881d9
felipe oliveira <felipeoliveira@felipes-MacBook-Air.local>
2025-09-21 21:05:48 -0300

    add auth & jwt token to application

--- src/main/java/med/voll/api/controller/AuthController.java
+++ src/main/java/med/voll/api/controller/AuthController.java
@@ -1,7 +1,10 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
+import med.voll.api.domain.users.User;
 import med.voll.api.domain.users.UserDataLogin;
+import med.voll.api.infra.security.TokenResponse;
+import med.voll.api.infra.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -19,15 +22,19 @@ public class AuthController {
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity login(@RequestBody @Valid UserDataLogin userDataLogin){
 
         /* -> Transforma os dados recebidos na request no objeto esperado para a funcao de auth (Instanciando essa funcao */
-        var token = new UsernamePasswordAuthenticationToken(userDataLogin.username(), userDataLogin.password());
-        var authentication = authenticationManager.authenticate(token);
+        var authenticationToken = new UsernamePasswordAuthenticationToken(userDataLogin.username(), userDataLogin.password());
+        var authentication = authenticationManager.authenticate(authenticationToken);
 
-        return ResponseEntity.ok().build();
+        var tokenJWT = tokenService.generateToken((User) authentication.getPrincipal());
 
+        return ResponseEntity.ok(new TokenResponse(tokenJWT));
     }
 
 
